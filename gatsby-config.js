a9a9cb646935c3e985cab8892965360dbf093a86
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-08-26 00:49:10 -0400

    fix: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `public/PersonalFavicon3.png`,
+        icon: `/public/PersonalFavicon3.png`,
       },
     },
   ],
